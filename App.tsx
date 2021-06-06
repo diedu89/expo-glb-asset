5e4147fb5ec0600b2ac5953d8567220e4de186ac
Diego Melendez <diedu89@gmail.com>
2021-06-06 00:41:38 -0600

    feat: code to load a custom extension asset

--- App.tsx
+++ App.tsx
@@ -1,14 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React, { useEffect } from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { Asset } from 'expo-asset';
 export default function App() {
+   useEffect(() => {
+     loadAsset();
+  }, [])
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>does this work</Text>
       <StatusBar style="auto" />
     </View>
   );
+}
+
+const loadAsset = async () => {
+  const moduleAsset = require('./assets/example.glb');
+  const asset = Asset.fromModule(moduleAsset);
+  await asset.downloadAsync();
+  console.log(asset.localUri);
 }
 
 const styles = StyleSheet.create({
